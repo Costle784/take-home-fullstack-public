1824075500788b7678ac48c59cadad4e94635d08
Curtis Ostle <curtisostle@gmail.com>
2021-05-21 09:03:24 -0400

    Update SQL statements

--- db/task1.sql
+++ db/task1.sql
@@ -1,9 +1,24 @@
 --- Please write SQL statements after the appropriate numbered lines below:
 
 -- 1.
+    SELECT u.user_id,
+        g.state_name,
+        g.state_code
+    FROM   ourdata.users u
+        JOIN ourdata.geo g
+            ON u.state = g.st_code;
 
 -- 2.
+    SELECT state,
+        Count(user_id) AS user_count
+    FROM   ourdata.users
+    GROUP  BY state;
 
 -- 3.
-
+    SELECT state,
+        Max(user_count)
+    FROM   (SELECT state,
+                Count(user_id) AS user_count
+            FROM   ourdata.users
+            GROUP  BY state) AS state_counts
 --
